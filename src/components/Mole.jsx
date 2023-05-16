76d2ba46d22e4fb0b1fde8ab7f89fd5922deba09
DanielRendon1020 <d.danny1020@gmail.com>
2023-05-15 20:32:52 -0700

    mole created

---
+++ src/components/Mole.jsx
@@ -0,0 +1,7 @@
+import mole from '../embeded/mole.png'
+
+export default function Mole() {
+    return <div>
+        <img src={mole} style={{ width: '30vw' }} />
+    </div>
+}
---
+++ src/components/MoleContainer.jsx
@@ -0,0 +1,7 @@
+import Mole from "./Mole"
+
+export default function MoleContainer() {
+    return <div className="mole-container">
+        <Mole />
+    </div>
+} 
