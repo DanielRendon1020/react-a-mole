cae9b4d0a1d29d3dfffe3c291202e13d2870cce6
DanielRendon1020 <d.danny1020@gmail.com>
2023-05-15 21:10:47 -0700

    one mole popping up

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -1,16 +1,15 @@
+import mole from '../embeded/molehill.png'
 import { useEffect } from 'react'
-import mole from '../embeded/mole.png'
 
-export default function Mole({setMoleIsUp}) {
+export default function Empty({setMoleIsUp}) {
     useEffect(() => {
-        let randMs = Math.ceil(Math.random() * 2000 + 500)
+        let randMs = Math.ceil(Math.random() * 2000 + 200)
         let timer = setTimeout(() => {
-            setMoleIsUp(false)
+            setMoleIsUp(true)
         }, randMs)
         return () => clearTimeout(timer)
     
     }, [])
-
     return <div>
         <img src={mole} style={{ width: '30vw' }} />
     </div>
--- src/components/MoleContainer.jsx
+++ src/components/MoleContainer.jsx
@@ -1,7 +1,11 @@
+import Empty from "./Empty"
 import Mole from "./Mole"
+import { useState } from "react"
 
 export default function MoleContainer() {
+    const [moleIsUp, setMoleIsUp] = useState(false)
+
     return <div className="mole-container">
-        <Mole />
+        {moleIsUp ? <Mole setMoleIsUp={setMoleIsUp} /> : <Empty setMoleIsUp={setMoleIsUp} />}
     </div>
 } 
